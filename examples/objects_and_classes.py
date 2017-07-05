89ac818ba75497a4e0493867ee241c97270a1060
isaacgr <isaacgrowell@gmail.com>
2017-07-05 15:38:18 -0400

    merged classes.py with objects_and_classes.py

--- examples/objects_and_classes.py
+++ examples/objects_and_classes.py
@@ -52,9 +52,30 @@ c2.attr = 10	#Create a new attribute attr for c2
 print((c2.real, c2.imag, c2.attr))
 c2.getData()
 
-c1.attr # c1 does not have this attribute
+# c1 does not have this attribute
 
 # Any attribute of an object ca n be delted anytime, using the del statement
 
+print('\n')
 
+#Example of using classes
+#Commented since I need to learn
 
+class Song(object): #A class tells python to make a new type of thing, the object is any instance of something (an instance is what you get when you tell python to create a class)
+
+	def __init__(self, lyrics):	#The __init__ method represents a constructor, when an object is created it gets passed to this method
+		self.lyrics = lyrics	#The self variable represents the instance of the object being accessed
+
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+
+happy_bday = Song(['Happy birthday to you',	#Set happy_bday to an instance of class Song
+				   'I dont want to get sued',
+				   'So I\'ll stop right there'])
+
+bulls_on_parade = Song(['This is another song',
+						'I should have typed out'])
+
+happy_bday.sing_me_a_song()	#From happy_bday get the sing_me_a_song funciton and call it with parameters self
+bulls_on_parade.sing_me_a_song()
