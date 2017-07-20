735457912383bba46c4217eda2f8e4347c42d264
isaacgr <isaacgrowell@gmail.com>
2017-07-20 07:14:59 -0400

    finished knn tutorial started svm

--- machine_learning/knn_from_scratch_2.py
+++ machine_learning/knn_from_scratch_2.py
@@ -23,38 +23,49 @@ def k_nearset_neighbors(data, predict, k=3):
     # most_common returns the first key which occurs the most times
     vote_result = Counter(votes).most_common(1)[0][0]
     # returns a list of lists where the fist element is the one that was most common
-    return vote_result
+    confidence = float(Counter(votes).most_common(1)[0][1]) / k
+    return vote_result, confidence
 
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace=True)
+for i in range(25):
+    accuracies = []
+    df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+    df.replace('?', -99999, inplace=True)
 
-df.drop(['id'], 1, inplace=True)
-full_data = df.astype(float).values.tolist()
-# get a list of lists
-random.shuffle(full_data)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+    # get a list of lists
+    random.shuffle(full_data)
 
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-#train data is everything except the last 20% of the data
-test_data = full_data[-int(test_size*len(full_data)):]
-#test_data is the last 20%
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
+    test_size = 0.2
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    #train data is everything except the last 20% of the data
+    test_data = full_data[-int(test_size*len(full_data)):]
+    #test_data is the last 20%
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
-correct = 0
-total =0
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearset_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        total += 1
+    correct = 0
+    total =0
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearset_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+#            else:
+#                print(confidence)
+            total += 1
 
-print('Accuracy: ', float(correct)/total)
-
+#    print('Accuracy: ', float(correct)/total)
+    accuracies.append(float(correct)/total)
+print(sum(accuracies)/len(accuracies))
 # one issue with knn is that to compare the closness of a point to the other datapoints, you have to
 # compare it to all datapoints in the set
+# KNN can give confidence
+# Accuracy is 'did we get the classification right?', confidence can come from the classifier
+
+# KNN can work on linear and non linear data
+# regression requires linear data
---
+++ machine_learning/svm_intro.py
@@ -0,0 +1,30 @@
+# The objective of the SVM is to find the best seperating hyperplane (aka decision boundary)
+# The greatest distance between the datapoints and the seperating hyperplace means its the best
+# Can classify new datapoints based on which side of the hyperplane that the data falls
+# It is a binary classifier, so it works on linear data
+
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors, svm
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True) # svm handles outliers slightly better than knn
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])   # we will be predicting this
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y, test_size = .2)
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
+
+example_measures = example_measures.reshape(len(example_measures),-1)
+prediction = clf.predict(example_measures)
+
+print(prediction)
