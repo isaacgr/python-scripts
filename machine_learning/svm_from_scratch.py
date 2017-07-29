42d94c68b2165ce26fefa445ed7147dbc0eef47f
isaacgr <isaacgrowell@gmail.com>
2017-07-29 11:33:01 -0400

    finished 2D svm_from_scratch tutorial

--- machine_learning/svm_from_scratch.py
+++ machine_learning/svm_from_scratch.py
@@ -75,7 +75,7 @@ class Support_Vector_Machine:
                         # yi(xi.w+b)>=1 is the constraint function
                             for xi in self.data[i]:
                                 yi=i
-                                if not yi(np.dot(w_t, xi)+b)>=1:
+                                if not yi*(np.dot(w_t, xi)+b)>=1:
                                     found_option = False
                                     break
                         if found_option:
@@ -92,19 +92,54 @@ class Support_Vector_Machine:
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0] + step*2
 
+            for i in self.data:
+                for xi in self.data[i]:
+                    yi=i
+                    print(xi, ':', yi*(np.dot(self.w, xi)+self.b))
+
     def predict(self, features):
         # sign(x.w+b)
         # we want to determine what the sign of the above equation is so we can classify the data
         classification = np.sign(np.dot(np.array(features), self.w)+self.b) # need to optimize for w and b
-        if classification !=0 and self.visulalization:
-            self.ax.scatter(features[0], features[1], s=200, marker = *, c=self.colors[classification])
+        if classification !=0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker = '*', c=self.colors[classification])
         return classification
 
     def visualize(self):
-        [self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i] for i in data_dict]
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
         # hyperplane = x.w + b
         # v = x.w +b
         def hyperplane(x,w,b, v):
+            return (-w[0]*x-b+v)/w[1]
+        #x,y is an unknown point on the hyperplane
+        #x_v and w_v are the vector
+        #x_v= [x,y]
+        #x_v.w_v+b =1 for postive sv
+        #x.w[0] + y.w[1] + b =1
+        #y = -x.w[0] - b + 1 / w[1]
+        #So we get our y co-ordinate to plot it.
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max],[psv1, psv2], 'k')
+
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max],[nsv1, nsv2], 'k')
+
+        # (w.x+b) = 0
+        # decision boundary
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max],[db1, db2], 'y--')
+        plt.show()  # need to do this durp
 
 # keys are the class, arrays are the features
 data_dict = {-1:np.array([[1,7,],
@@ -113,3 +148,13 @@ data_dict = {-1:np.array([[1,7,],
               1:np.array([[5,1],
                           [6,-1],
                           [7,3]])}
+
+svm = Support_Vector_Machine()
+svm.fit(data_dict)
+
+predict_us = [[0,10], [1,3], [3,4], [3,5], [5,5], [5,6], [6,-5], [5,8]]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
