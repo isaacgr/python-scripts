3dc05dbcde51775517c211297df2eacb0cdc2032
isaacgr <isaacgrowell@gmail.com>
2017-08-01 07:41:55 -0400

    finished kernels example no real code for that one

--- machine_learning/kernels.py
+++ machine_learning/kernels.py
@@ -16,3 +16,14 @@
 # given a feature set [x1,x2] the new z space is z=[1,x1,x2,x1^2, x2^2, x1x2]
 # we convert the feature set to a second order polynomial, then take the inner product of z and z`
 # we can now say that K(x,x`) = (1+x*x`)^p where p=2
+
+# with non-linear data you want to watch out for overfitment
+# having a lot of data that perfectly fit support vector lines
+# better to have less data that is actually a sv
+# In overfitting, a statistical model describes random error or noise instead of the underlying relationship
+
+# soft margin svm has some error between seperating data points
+# can introduce a new variable, Slack (e)
+# so now the equeation of our svm is yi(xi.w +b) >= 1 - e
+# we want to minimize the slack, minimize = 1/2||w||^2 + C*sum(e)
+# if we raise C, then we are going to punish more for violations of the margin
