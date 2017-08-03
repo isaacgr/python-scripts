63a2178650243a614b3265e13d695f756e40ad02
isaacgr <isaacgrowell@gmail.com>
2017-08-03 07:30:54 -0400

    finished tutorial on clustering with non numerical data

--- machine_learning/clustering_intro.py
+++ machine_learning/clustering_intro.py
@@ -24,7 +24,7 @@ X = np.array([[1,2], [1.5,1.8], [5,8], [8,8], [1, 0.6], [9,11]])
 #plt.scatter(X[:, 0], X[:, 1], s=100, linewidths=5)
 #plt.show()
 
-clf = KMeans(n_clusters=6)
+clf = KMeans(n_clusters=2)
 clf.fit(X)
 
 centroids = clf.cluster_centers_
