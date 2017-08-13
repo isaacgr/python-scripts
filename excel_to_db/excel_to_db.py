692557f45e7332d443199155a70c6f04abd0a5c9
isaacgr <isaacgrowell@gmail.com>
2017-08-13 03:15:55 -0400

    Require Serial Number to insert data. Removed some comments.

--- excel_to_db/excel_to_db.py
+++ excel_to_db/excel_to_db.py
@@ -3,11 +3,10 @@ import MySQLdb
 
 RESULTS_SHEET = "Compiled Tests.xlsx"
 MATERIALS_SHEET = ""
-
-TEST_RESULT_COLUMNS = ['Capacitance', 'Charge_Count', 'Cycles', 'Pin']
-CAPACITOR_COLUMNS = ['Serial_Number']
-MATERIAL_COLUMNS = ['Electrolyte', 'Seperator', 'DPI', 'Form_Factor']
-
+# columns that exist in the database tables
+TEST_RESULT_COLUMNS = ['Serial_Number','Capacitance', 'Charge_Count', 'Cycles', 'Pin', 'Date_Tested']
+MATERIAL_COLUMNS = ['Serial_Number', 'Electrolyte', 'Seperator', 'DPI', 'Form_Factor']
+# connections to various databases if needed
 DATABASE_CONNS = {
     # DB Name : Credentials
     'CapacitorTests': {
@@ -24,8 +23,9 @@ class DatabaseInsert:
         self.workbook = workbook
         self.database = database
         self.workbook = xlrd.open_workbook(workbook)
-        self.SHEETS = []
-        [self.SHEETS.append(self.workbook.sheet_by_name(str(i))) for i in self.workbook.sheet_names()]
+        self.sheet_obj = []
+        self.sheet_names = self.workbook.sheet_names()
+        [self.sheet_obj.append(self.workbook.sheet_by_name(str(i))) for i in self.workbook.sheet_names()]
 
     # Establish connection to databse and enable the cursor, which will
     # walk line by line through the DB and assign values
@@ -33,24 +33,20 @@ class DatabaseInsert:
         host, passwd, db, user = self.database.values()
         self.db_connection = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
         self.cursor = self.db_connection.cursor()
-        # get names of columns from the database
-        #num_fields = len(cursor.description)
-        #field_names = [i[0] for i in cursor.description]
-        #print(field_names)
-        #return field_names
-
-
     # This function needs to be changed on a per sheet basis until
     # excel sheet headers are updated
-    def insert_to_database(self, table, columns, query):
+    def insert_results_to_database(self, table, columns, query):
         self.connect_to_db()
-        #column_names = self.connect_to_db()
         values = {}
-
-        for sheet in self.SHEETS[1:]:
-            values['Date_Tested'] = self.SHEETS.index(sheet)
+        # loop through spreadsheet and get the values from the columns and rows
+        for sheet in self.sheet_obj[1:]:
+            values['Date_Tested'] = self.workbook.sheet_names()[self.sheet_obj.index(sheet)]
             for column in range(0, sheet.ncols)[::3][:-1]:
                 values['Serial_Number'] = sheet.cell(0,column).value
+                # restart loop if there is no serial number
+                if values['Serial_Number'] == '':
+                    continue
+
                 values['Pin'] = str(sheet.cell(1, column).value)
                 for row in range(2, sheet.nrows):
                     values["Capacitance"] = sheet.cell(row, column).value
@@ -60,21 +56,18 @@ class DatabaseInsert:
                     for key in values.keys():
                         if key in columns:
                             db_columns.append(values[key])
-
+                    # write values to the db
                     self.cursor.execute(query, tuple(db_columns))
-
+        # commit the changes and close the db connection
         self.cursor.close()
         self.db_connection.commit()
         self.db_connection.close()
         print 'Wrote %s Rows to %s Columns for Table %s' % (sheet.nrows, sheet.ncols, table)
 
-#query = """INSERT INTO {0} {1} VALUES {2}""".format(table, tuple(db_columns), tuple(values.values()))
-
 results = DatabaseInsert(RESULTS_SHEET, DATABASE_CONNS['CapacitorTests'])
 
-results.insert_to_database('TestResults', TEST_RESULT_COLUMNS, \
-                """INSERT INTO TestResults (Pin, Charge_Count, Capacitance, Cycles) VALUES (%s, %s, %s, %s)""")
+results.insert_results_to_database('TestResults', TEST_RESULT_COLUMNS, \
+                """INSERT INTO TestResults (Pin, Date_Tested, Charge_Count, Capacitance, Serial_Number, Cycles) \
+                    VALUES (%s, %s, %s, %s, %s, %s)""")
 
-results.insert_to_database('Capacitor', CAPACITOR_COLUMNS, """INSERT INTO Capacitor (Serial_Number) VALUES (%s)""")
-
-#results.insert_to_database('Materials', MATERIAL_COLUMNS)
+#results.insert_materials_to_database('Materials', MATERIAL_COLUMNS)
