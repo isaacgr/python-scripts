a70152547540e64fe0659abfbae7365d2af16fc2
isaacgr <isaacgrowell@gmail.com>
2017-08-17 08:04:13 -0400

    Handle duplicate serial number

--- excel_to_json/excel_to_json.py
+++ excel_to_json/excel_to_json.py
@@ -14,11 +14,18 @@ class JsonConversion:
         self.sheet_names = self.workbook.sheet_names()
         [self.sheet_obj.append(self.workbook.sheet_by_name(str(i))) for i in self.workbook.sheet_names()]
 
-
+    # TODO: Format excel sheets so this can be made to be more generic
     def convert_results_to_json(self):
         results = {}
         values = {}
         results['capacitor'] = []
+
+        values['Capacitance'] = []
+        values['Charge_Count'] = []
+        values['Cycles'] = []
+        values['Pin'] = []
+
+
         # loop through spreadsheet and get the results from the columns and rows
         for sheet in self.sheet_obj[1:]:
             values['Date_Tested'] = self.workbook.sheet_names()[self.sheet_obj.index(sheet)]
@@ -27,33 +34,45 @@ class JsonConversion:
                 # restart loop if there is no serial number
                 if values['Serial_Number'] == '':
                     continue
-
+                # pin almost as unique as serial, since only one cap on one pin at a time
                 values['Pin'] = str(sheet.cell(1, column).value)
                 for row in range(2, sheet.nrows):
-                    values['Capacitance'] = sheet.cell(row, column).value
-                    values['Charge_Count'] = sheet.cell(row, column + 1).value
-                    values['Cycles'] = sheet.cell(row, column + 2).value
+                    values['Capacitance'].append(sheet.cell(row, column).value)
+                    values['Charge_Count'].append(sheet.cell(row, column + 1).value)
+                    values['Cycles'].append(sheet.cell(row, column + 2).value)
 
-                    if values['Serial_Number'] in values.keys():
-                        self.serial = values['Serial_Number']
-                        handle_serial_number(self.serial)
+                if values['Serial_Number'] in values.values():
+                    serial = values['Serial_Number']
+                    handle_duplicate_serial_number(serial, values)
+                    continue
 
-                    results['results'].append({
+                serial_number = values['Serial_Number']
 
-                        'capacitor': {
+                results['results'].append({
 
-                            'Serial_Number':values['Serial_Number'],
-                            'Date_Tested':values['Date_Tested'],
-                            'Capacitance':values['Capacitance'],
-                            'Charge_Count':values['Charge_Count'],
-                            'Cycles':values['Cycles'],
+                    serial_number: [{
 
-                        }
-                    })
+                        'Date_Tested':values['Date_Tested'],
+                        'Capacitance':values['Capacitance'],
+                        'Charge_Count':values['Charge_Count'],
+                        'Cycles':values['Cycles'],
+                        'Pin':values['Pin'],
 
-
-    def handle_serial_number(serial):
-
+                    }]
+                })
 
 
-        print 'Wrote %s Rows from %s Columns' % (sheet.nrows, sheet.ncols)
+    def handle_duplicate_serial_number(serial, values):
+
+        results['results'][serial].append({
+
+            'Date_Tested':values['Date_Tested'],
+            'Capacitance':values['Capacitance'],
+            'Charge_Count':values['Charge_Count'],
+            'Cycles':values['Cycles'],
+            'Pin':values['Pin'],
+
+        })
+
+
+        print 'Wrote %s Rows from %s Columns (%s lines)' % (sheet.nrows, sheet.ncols, (sheet.nrows*sheet.ncols))
