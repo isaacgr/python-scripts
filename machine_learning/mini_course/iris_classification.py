13a2255ec4f68b44f9d7d48d9c2c01208f665041
isaacgr <isaacgrowell@gmail.com>
2017-09-09 20:34:01 -0400

    Finished iris flower classification tutorial

--- machine_learning/mini_course/iris_classification.py
+++ machine_learning/mini_course/iris_classification.py
@@ -1,5 +1,5 @@
 import pandas
-from pandas.tools.plotting import scatter_matrix
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
@@ -38,3 +38,65 @@ dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False
 # will see that at least two of the input variables have a Gaussian distribution
 # can use algorithms to exploit this in the future
 dataset.hist()
+# can look at scatter plots of all pairs of attributes
+# the diagonal grouping of some pairs of attributes suggests a high correlation
+scatter_matrix(dataset)
+
+# we are going to split the model into two
+# 80% used to train the model and 20% to hold back as a validation set
+# allows us to estimate the accuracy of the models that we create on unseen data
+
+random_rows = dataset.sample(frac=1)    # randomize the rows so data does not show in order
+array = random_rows.values  # get all the values
+
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,Y, test_size=validation_size, random_state=seed)
+
+# we ill use 10-fold cross validation, which will split the dataset into 10 parts
+# it will train on 9 and test on 1 then repeat for all combinations of train-test splits
+scoring = 'accuracy'
+
+# create an array of machine learning models to test
+models = []
+#linear
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+# non-linear
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('LSVM', SVC()))
+# evaluate each model
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)   # provides train/test indicies to split datasets
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" %(name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# plot algorithms and compare accuracy
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+
+# KNN was the most accurate model so we want to get an idea of the accuracy on our validation set
+# its important to keep a validation set in case you overfit to the training data
+# we can run the model on the validation set and summarize the results as a
+# final accuracy score, confusion matrix and classifcation report
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+# a confusion matrix is a technique to summarize the performance of a classification algorithms
+# gives a better idea of what the model is getting right and what types of errors its making
+# shows the ways in which your model iss confused when it makes predictions
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
