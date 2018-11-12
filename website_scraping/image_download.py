8b52659441c67686b2e3389dd97ca4b42a375d85
isaacgr <isaacgrowell@gmail.com>
2018-11-11 21:21:20 -0500

    Use better html parser Loop through user provided number of pages

--- website_scraping/image_download.py
+++ website_scraping/image_download.py
@@ -7,46 +7,43 @@ import requests
 import shutil
 
 # url = raw_input("[+] Enter the URL: ")
-url = "https://www.artic.edu/collection?is_public_domain=1"
+url = "https://www.artic.edu/collection/more?is_public_domain=1"
 download_path = raw_input("[+] Enter the full download path: ")
+pages = raw_input("[+] Enter the number of pages to fetch: ")
+i = 0
 
-try:
-    # headers = {"User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
-    i = 0
+for page in range(2, int(pages) + 1):
+    try:
+        print '[*] Requesting images'
+        r = requests.get(url + '&page=%s' % page)
+        soup = BeautifulSoup(r.content, 'lxml')
+        for tag in soup.find_all('img'):
+            try:
+                if tag.attrs['data-srcset']:
+                    image_url = tag.attrs['data-srcset'].replace('\\', '').replace('"','')
+                    if os.path.splitext(os.path.basename(image_url))[1] == '.jpg':
+                        current = requests.get(image_url, stream=True)
+                        print "\n[*] Downloading: %s" % (os.path.basename(image_url))
+                        with open(download_path + str(i) + '.jpg', 'wb') as f:
+                            current.raw.decode_content = True
+                            shutil.copyfileobj(current.raw, f)    
+                            f.close()
+                        i += 1
+            except:
+                print '[*] Could not download image'
+                continue
 
-    # request = urllib2.Request(url, None, headers)
-    r = requests.get(url, verify=False)
-    # html = urllib2.urlopen(request)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    for tag in soup.findAll('img'):
-        try:
-            if tag.attrs['data-srcset']:
-                image_urls = tag.attrs['data-srcset']
-                image_url = image_urls.split()[0]
-                # tag.attrs['data-srcset'] = urlparse.urljoin(url, tag['srcset'])
-                if os.path.splitext(os.path.basename(image_url))[1] == '.jpg':
-                    current = requests.get(image_url, verify=False, stream=True)
-                    print "\n[*] Downloading: %s" % (os.path.basename(image_url))
-                    # f = open(download_path + 'default' + str(i) + '.jpg', 'wb')
-                    with open(download_path + 'default' + str(i) + '.jpg', 'wb') as f:
-                        current.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)    
-                    f.close
-                    i += 1
-        except Exception as e:
-            print e
-            continue
+        print "\n[*] Downloaded %d files" %(i+1)
 
-    print "\n[*] Downloaded %d files" %(i+1)
-    raw_input("[+] Press any key to exit...")
+    except KeyboardInterrupt:
+        print "[*] Exiting"
 
-except KeyboardInterrupt:
-    print "[*] Exiting"
+    except SyntaxError:
+        print "[*] Fix your code stupid"
+        sys.exit(1)
 
-except SyntaxError:
-    print "[*] Fix your code stupid"
-    sys.exit(1)
+    except:
+        print "[*] Could not get information from server %s" % e
+        continue
 
-except Exception as e:
-    print "[*] Could not get information from server %s" % e
-    sys.exit(2)
+raw_input('[+] Press any key to exit')
